0571d77fb5a5766f7d2e0ef384c16e49e5c278a2
Blake Hildebrand <baugusthildebrand@gmail.com>
2022-09-25 15:27:54 -0400

    Hook up CLI to add user inputs

--- foxhole-production-calculator-cli/src/main.rs
+++ foxhole-production-calculator-cli/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use clap::Parser;
 use foxhole_production_calculator::ResourceGraph;
 use foxhole_production_calculator_types::Material;
@@ -16,13 +14,24 @@ struct Args {
     /// Specifies the desired rate of output for the given material. [Unit/Hour]
     #[clap(value_parser)]
     rate: u64,
+
+    /// Optional argument specifying inputs that will be brought in externally from the factory.
+    /// Multiple values can be input with comma seperators.
+    #[clap(short, long, value_parser, use_value_delimiter = true)]
+    user_inputs: Option<Vec<Material>>,
 }
 
 fn main() {
     let args = Args::parse();
 
     let rg = ResourceGraph::default();
-    let reqs = rg.calculate_factory_requirements(args.material, args.rate, HashSet::new());
+
+    let user_inputs = args.user_inputs.unwrap_or_else(Vec::new);
+    let reqs = rg.calculate_factory_requirements(
+        args.material,
+        args.rate,
+        user_inputs.into_iter().collect(),
+    );
 
     println!("{}", serde_json::to_string_pretty(&reqs).unwrap());
 }
