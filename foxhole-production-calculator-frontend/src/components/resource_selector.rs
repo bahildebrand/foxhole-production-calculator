0581ba160395317351e7050475a36a75a0663e98
Blake Hildebrand <baugusthildebrand@gmail.com>
2022-10-01 22:22:50 -0400

    Fix panic caused by unwrapping invalid integer

--- foxhole-production-calculator-frontend/src/components/resource_selector.rs
+++ foxhole-production-calculator-frontend/src/components/resource_selector.rs
@@ -2,13 +2,14 @@ use std::str::FromStr;
 
 use foxhole_production_calculator_types::Material;
 use strum::IntoEnumIterator;
-use web_sys::{HtmlInputElement, HtmlSelectElement};
+use web_sys::{HtmlButtonElement, HtmlInputElement, HtmlSelectElement};
 use yew::prelude::*;
 
 use crate::CalculationClickedArgs;
 
 pub enum ResourceSelectionMsg {
     Calculate,
+    InputChanged,
 }
 
 #[derive(Clone, PartialEq, Properties)]
@@ -19,6 +20,8 @@ pub struct ResourceSelectionProps {
 pub struct ResourceSelection {
     material_ref: NodeRef,
     input_ref: NodeRef,
+    button_ref: NodeRef,
+    rate: u64,
 }
 
 impl Component for ResourceSelection {
@@ -29,27 +32,51 @@ impl Component for ResourceSelection {
         Self {
             material_ref: NodeRef::default(),
             input_ref: NodeRef::default(),
+            button_ref: NodeRef::default(),
+            rate: 0,
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             ResourceSelectionMsg::Calculate => {
-                let input_element = self.input_ref.cast::<HtmlInputElement>();
                 let material_element = self.material_ref.cast::<HtmlSelectElement>();
-                if let (Some(input_element), Some(material_element)) =
-                    (input_element, material_element)
-                {
-                    // FIXME: actually handle this, currently will crash on bad user input
-                    let rate = input_element.value().parse::<u64>().unwrap();
-                    // This should always match, should probably handle this better
+                if let Some(material_element) = material_element {
+                    // This should always match
                     let material = Material::from_str(&material_element.value()).unwrap();
 
-                    let callback_args = CalculationClickedArgs { material, rate };
+                    let callback_args = CalculationClickedArgs {
+                        material,
+                        rate: self.rate,
+                    };
                     ctx.props().calculation_callback.emit(callback_args);
 
                     true
                 } else {
+                    false
+                }
+            }
+            ResourceSelectionMsg::InputChanged => {
+                log::info!("Changed");
+                let input_element = self.input_ref.cast::<HtmlInputElement>();
+                let button_element = self.button_ref.cast::<HtmlButtonElement>();
+                if let (Some(input_element), Some(button_element)) = (input_element, button_element)
+                {
+                    if let Ok(rate) = input_element.value().parse::<u64>() {
+                        self.rate = rate;
+                        button_element.set_disabled(false);
+                        input_element.set_class_name("input");
+
+                        false
+                    } else {
+                        input_element.set_placeholder("Invalid input");
+                        input_element.set_class_name("input is-danger");
+                        button_element.set_disabled(true);
+
+                        true
+                    }
+                } else {
+                    log::error!("Could not find resource selection input element");
                     false
                 }
             }
@@ -74,14 +101,16 @@ impl Component for ResourceSelection {
                     </div>
                 </div>
                 <div class="control">
-                    // FIXME: Make input only take positive integers
-                    <input class="input" type="text" placeholder="100" ref={self.input_ref.clone()}/>
+                    <input class="input" type="text" placeholder="Unit/Hour"
+                        ref={self.input_ref.clone()}
+                        onchange={link.callback(|_| ResourceSelectionMsg::InputChanged)}/>
                 </div>
             </div>
             <div class="field">
                 <div class="control">
                     <button
                         class="button is-link"
+                        ref={self.button_ref.clone()}
                         onclick={link.callback(|_| ResourceSelectionMsg::Calculate)}
                         >
                         { "Calculate" }
